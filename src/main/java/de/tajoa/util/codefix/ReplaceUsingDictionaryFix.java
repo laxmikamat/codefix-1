7e72229610dbc55ee60535888d74c96dc4404e04
jonas.tangermann <jonas.tangermann@yahoo.de>
2017-05-12 16:03:25 +0200

    Show Surrounding option

--- src/main/java/de/tajoa/util/codefix/ReplaceUsingDictionaryFix.java
+++ src/main/java/de/tajoa/util/codefix/ReplaceUsingDictionaryFix.java
@@ -4,21 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class ReplaceUsingDictionaryFix implements Fix {
@@ -29,8 +24,10 @@ public class ReplaceUsingDictionaryFix implements Fix {
 	private final Dictionary dictionary;
 	private final Collection<String> replacements;
 
-	public ReplaceUsingDictionaryFix(String toBeReplaced, Charset charset, Dictionary dictionary,
-			String... replacements) {
+	public ReplaceUsingDictionaryFix(	String toBeReplaced,
+										Charset charset,
+										Dictionary dictionary,
+										String... replacements) {
 		this.toBeReplaced = toBeReplaced;
 		this.wordBoundRegex = toWordBoundRegex(toBeReplaced);
 		this.charset = charset;
@@ -68,12 +65,16 @@ public class ReplaceUsingDictionaryFix implements Fix {
 		return input;
 	}
 
-	private String handleAmbigiusReplacement(String input, String matchedWord, List<String> possibleReplacements) {
-		System.out.println("Multiple possible replacements found for: " + matchedWord + ", " + possibleReplacements);
-		System.out.println("Please enter the index of your choice:");
+	private String handleAmbigiusReplacement(	String input,
+												String matchedWord,
+												List<String> possibleReplacements) {
+		System.out.println("Multiple possible replacements found for: " + matchedWord + ", "
+				+ possibleReplacements);
+		System.out.println("Please enter the index of your choice. Type \"0\" to show the surrounding text:");
 		try {
 			int choice = this.getIntFromSysIn();
-			String replacement = possibleReplacements.get(choice);
+			choice = handleSourrundingChoice(input, matchedWord, choice);
+			String replacement = possibleReplacements.get(choice - 1);
 			System.out.println("Replacing: " + matchedWord + " -> " + replacement);
 			return input.replaceFirst(wordBoundRegex, replacement);
 		} catch (Exception e) {
@@ -82,14 +83,34 @@ public class ReplaceUsingDictionaryFix implements Fix {
 		return handleAmbigiusReplacement(input, matchedWord, possibleReplacements);
 	}
 
+	private int handleSourrundingChoice(String input,
+										String matchedWord,
+										int choice) throws IOException {
+		if (choice == 0) {
+			int indexOfMatch = input.indexOf(matchedWord);
+			String sd = input.substring(Math.max(0, indexOfMatch - 50),
+										Math.min(input.length(), indexOfMatch + 50));
+			System.out.println("Surrounding of Match: \n" + sd);
+			System.out.println("Please enter the index of your choice:");
+			choice = this.getIntFromSysIn();
+		}
+		return choice;
+	}
+
 	private static String toWordBoundRegex(String toBeReplaced) {
 		return "\\w*" + toBeReplaced + "\\w*";
 	}
 
 	public List<String> possibleReplacements(String matchedWord) {
-		return this.replacements.stream().map(r -> matchedWord.replaceFirst(toBeReplaced, r))
-				.filter(ty -> dictionary.contains(ty)).collect(Collectors.toList());
+		return this.replacements.stream()
+								.map(r -> matchedWord.replaceFirst(toBeReplaced, r))
+								.filter(ty -> dictionary.contains(ty)
+										|| (StringUtils.isAllLowerCase(ty.substring(1))
+												&& dictionary.contains(ty.toLowerCase())))
+								.collect(Collectors.toList());
 	}
+
+	private HashMap<String[], String> memFix;
 
 	public int getIntFromSysIn() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -98,8 +119,8 @@ public class ReplaceUsingDictionaryFix implements Fix {
 
 	@Override
 	public String toString() {
-		return "ReplaceUsingDictionaryFix [toBeReplaced=" + toBeReplaced
-				+ ", charset=" + charset + ", replacements=" + replacements + "]";
+		return "ReplaceUsingDictionaryFix [toBeReplaced=" + toBeReplaced + ", charset=" + charset
+				+ ", replacements=" + replacements + "]";
 	}
 
 }
